9ce5e08931839c2330a28964ee73f7182053e69e
simpleapples <zangzhiya@gmail.com>
2014-09-10 15:05:47 +0800

    fix

--- ShakeChoose/MainViewController.m
+++ ShakeChoose/MainViewController.m
@@ -30,12 +30,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self updateViewWithInit:YES];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [self updateViewWithInit:YES];
     [self becomeFirstResponder];
 }
 
